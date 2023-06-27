12cab35a22b2b14baa0e9668742a68b9df5b9bad
TyFhoon <110676106+TyFhoon@users.noreply.github.com>
2023-06-27 11:30:25 +0100

    Update Unity_Servo.ino

--- Unity_Servo.ino
+++ Unity_Servo.ino
@@ -4,21 +4,63 @@ int data;
 int temp;
 
 Servo myservo;
-int pos = 0; //Position of servo
 
-void setup() {
+int pos; //Position of servo
+
+void setup() 
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(2,OUTPUT);
   pinMode(12,OUTPUT);
   pinMode(A0,INPUT);
   myservo.attach(9);
+  pos = 0;
 }
 
-void WriteServo()
+void CheckServoState()
 {
-  myservo.write(pos);
-  delay(15);
+  //==========================
+  // UPDATE SERVO FOR UNITY
+  //==========================
+  if (myservo.attached() == true)
+  {
+    if (pos == 0)
+    {
+      Serial.println("Servo,0");
+    }
+    else if (pos == 180)
+    {
+      Serial.println("Servo,180");
+    }
+    else if (pos == 1 || pos == 179) 
+    {
+      Serial.println("Servo,Changing");
+    }
+  }
+  else
+  {
+    Serial.println("Servo,Off");
+  }
+}
+
+void CheckTempState()
+{
+  if (digitalRead(12) == HIGH)
+  {
+    int reading = analogRead(A0);
+    // Convert that reading into voltage
+    // Replace 5.0 with 3.3, if you are using a 3.3V Arduino
+    float voltage = reading * (5.0 / 1024.0);
+    // Convert the voltage into the temperature in Celsius
+    float temperatureC = (voltage - 0.5) * 100;
+    Serial.print("Temp,");
+    Serial.println(temperatureC);
+  }
+  else if (digitalRead(12) == LOW)
+  {
+    Serial.println("Temp,Off");
+  }
 }
 
 void loop() 
@@ -60,64 +102,49 @@ void loop()
     //CONTROL SERVO TO 180
     //=====================
 
-    if (data == "S") //S for servo //"S" == RIGHT
+    if (data == 'S') //S for servo //"S" == RIGHT
     {
-      for (pos = 0; pos <= 180; pos += 1)
+      for (int count = 0; count <= 180; count += 1)
       {
-        WriteServo();
+        pos = count;
+        myservo.write(pos);
+        delay(15);
+        CheckServoState();
+
+        //While servo is checked, the temp also needs to be checked for simultaneous updated
+        if (count%20 == 0)
+        {
+          CheckTempState();
+        }
       }
     }
     //=====================
     //CONTROL SERVO TO 0
     //=====================
 
-    else if (data == "s") // "s" == LEFT
+    else if (data == 's') // "s" == LEFT
     {
-      for (pos = 180; pos >= 0; pos -= 1)
+      for (int count = 180; count >= 0; count -= 1)
       {
-        WriteServo();
-      }
+        pos = count;
+        myservo.write(pos);
+        delay(25);
+        CheckServoState();
+
+        //While servo is checked, the temp also needs to be checked for simultaneous updated
+        if (count%20 == 0)
+        {
+          CheckTempState();
+        }
+        
+  }
     }
   }
   //==========================
   // UPDATE TEMP FOR UNITY
   //==========================
 
-  if (digitalRead(12) == HIGH)
-  {
-    int reading = analogRead(A0);
-    // Convert that reading into voltage
-    // Replace 5.0 with 3.3, if you are using a 3.3V Arduino
-    float voltage = reading * (5.0 / 1024.0);
-    // Convert the voltage into the temperature in Celsius
-    float temperatureC = (voltage - 0.5) * 100;
-    Serial.println("Temp,", temperatureC);
-  }
-  else if (digitalRead(12) == LOW)
-  {
-    Serial.println("Temp,Off");
-  }
-  //==========================
-  // UPDATE SERVO FOR UNITY
-  //==========================
-  if (myservo.attached() == true)
-  {
-    if (pos == 0)
-    {
-      Serial.println("Servo,0");
-    }
-    else if (pos == 180)
-    {
-      Serial.println("Servo,180");
-    }
-    else if (pos != 180 && pos != 0)
-    {
-      Serial.println("Servo,Changing");
-    }
-  }
-  else
-  {
-    Serial.println("Servo,Off");
-  }
-  delay(250);
+  CheckTempState();
+  
+  delay(500);
 }
