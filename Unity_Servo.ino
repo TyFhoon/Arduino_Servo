b82b938080687c0d1fdc6762a76c344b5e47dbea
TyFhoon <110676106+TyFhoon@users.noreply.github.com>
2023-06-28 11:11:44 +0100

    Update Unity_Servo.cs

--- Unity_Servo.ino
+++ Unity_Servo.ino
@@ -1,150 +1,95 @@
-#include <Servo.h>
+using System.Collections;
+using UnityEngine;
+using System;
+using System.IO;
+using System.IO.Ports;
+using TMPro;
 
-int data;
-int temp;
-
-Servo myservo;
-
-int pos; //Position of servo
-
-void setup() 
+public class Ard_Script : MonoBehaviour
 {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(2,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(A0,INPUT);
-  myservo.attach(9);
-  pos = 0;
-}
+    //Connects the Serial port of the arduino to unity
+    public SerialPort serial = new SerialPort("COM3", 9600);
+    public TMP_Text Temp_Output;
+    public TMP_Text Servo_Output;
 
-void CheckServoState()
-{
-  //==========================
-  // UPDATE SERVO FOR UNITY
-  //==========================
-  if (myservo.attached() == true)
-  {
-    if (pos == 0)
+    //gets the temperature from the arduino
+    private string getLine;
+    //After getting the value from Arduino, 2 values are outputted, the sensor and its reading
+    private string OutputSensor;
+    private string OutputValue;
+
+    void Start()
     {
-      Serial.println("Servo,0");
+        serial.Open();
     }
-    else if (pos == 180)
+
+    void OpenSerial()
     {
-      Serial.println("Servo,180");
-    }
-    else if (pos == 1 || pos == 179) 
-    {
-      Serial.println("Servo,Changing");
-    }
-  }
-  else
-  {
-    Serial.println("Servo,Off");
-  }
-}
-
-void CheckTempState()
-{
-  if (digitalRead(12) == HIGH)
-  {
-    int reading = analogRead(A0);
-    // Convert that reading into voltage
-    // Replace 5.0 with 3.3, if you are using a 3.3V Arduino
-    float voltage = reading * (5.0 / 1024.0);
-    // Convert the voltage into the temperature in Celsius
-    float temperatureC = (voltage - 0.5) * 100;
-    Serial.print("Temp,");
-    Serial.println(temperatureC);
-  }
-  else if (digitalRead(12) == LOW)
-  {
-    Serial.println("Temp,Off");
-  }
-}
-
-void loop() 
-{
-  if (Serial.available())
-  {
-    //============================
-    //       TEMP AND LED
-    //============================
-    //=====================
-    //TURN ON TEMP AND LED
-    //=====================
-
-    data = Serial.read();
-    if (data == 'T') //T for Temp
-    {
-      //Turn on light
-      digitalWrite(2,HIGH);
-      //Turn on temp
-      digitalWrite(12,HIGH);
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
-    //=====================
-    //TURN OFF TEMP AND LED
-    //=====================
-
-    else if (data == 't')
-    {
-      //Turn off light
-      digitalWrite(2,LOW);
-      //turn off temp
-      digitalWrite(12,LOW);
-      digitalWrite(LED_BUILTIN, LOW);
-    }
-    //============================
-    //       SERVO CONTROL
-    //============================
-    //=====================
-    //CONTROL SERVO TO 180
-    //=====================
-
-    if (data == 'S') //S for servo //"S" == RIGHT
-    {
-      for (int count = 0; count <= 180; count += 1)
-      {
-        pos = count;
-        myservo.write(pos);
-        delay(15);
-        CheckServoState();
-
-        //While servo is checked, the temp also needs to be checked for simultaneous updated
-        if (count%20 == 0)
+        if (serial.IsOpen == false)
         {
-          CheckTempState();
+            serial.Open();
         }
-      }
     }
-    //=====================
-    //CONTROL SERVO TO 0
-    //=====================
 
-    else if (data == 's') // "s" == LEFT
+    //Checks if this get line is for the right sensor (Temp or servo)
+    void CheckSensor()
     {
-      for (int count = 180; count >= 0; count -= 1)
-      {
-        pos = count;
-        myservo.write(pos);
-        delay(25);
-        CheckServoState();
-
-        //While servo is checked, the temp also needs to be checked for simultaneous updated
-        if (count%20 == 0)
-        {
-          CheckTempState();
-        }
-        
-  }
+        getLine = serial.ReadLine();
+        Debug.Log(getLine);
+        string[] splitGetLine = getLine.Split(',');
+        OutputSensor = splitGetLine[0];
+        OutputValue = splitGetLine[1];
     }
-  }
-  //==========================
-  // UPDATE TEMP FOR UNITY
-  //==========================
 
-  CheckTempState();
-  
-  delay(500);
+    void Update()
+    {
+        //Outputs the temperature
+        CheckSensor();
+        if (OutputSensor == "Temp")
+        {
+            Temp_Output.text = OutputValue;
+        }
+        else if (OutputSensor == "Servo")
+        {
+            Servo_Output.text = OutputValue;
+        }
+    }
+    //ON BUTTON
+    public void OnFunc()
+    {
+        OpenSerial();
+        //Sends "A" to arduino to turn ON
+        serial.Write("T");
+        Debug.Log("On");
+    }
+    //OFF BUTTON
+    public void OffFunc()
+    {
+        OpenSerial();
+        //Sends "a" to arduino to turn OFF
+        serial.Write("t");
+        Debug.Log("Off");
+    }
+    //RIGHT BUTTON
+    public void RightFunc()
+    {
+        if (serial.IsOpen == false)
+        {
+            serial.Open();
+        }
+        //Sends "S" to arduino to make the servo go Right
+        serial.Write("S");
+        Debug.Log("Right");
+    }
+    //LEFT BUTTON
+    public void LeftFunc()
+    {
+        if (serial.IsOpen == false)
+        {
+            serial.Open();
+        }
+        //Sends "s" to arduino to make the servo go left
+        serial.Write("s");
+        Debug.Log("Left");
+    }
 }
